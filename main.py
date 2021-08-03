232913a77dbe9215962b351fae6419db77ed08a4
0217tatsunoko <>
2021-08-03 03:37:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+import random
+from keep_alive import keep_alive
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('hello'):
+    await message.channel.send('hello!!!!')
+  if message.content.startswith('1D10'):
+    a=random.randint(1,10)
+    await message.channel.send(a)
+keep_alive()
+client.run(os.getenv('TOKEN'))
